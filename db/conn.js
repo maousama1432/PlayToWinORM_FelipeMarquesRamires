48dab49e3316f2380d867d042e7c5f3dd26bb9d6
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2024-08-23 10:02:13 -0400

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,14 @@
-const { Sequelize } = require("sequelize");
+const {Sequelize} = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false, // Desativa os logs de SQL para tornar o console mais limpo
-  }
+    process.env.DB_DATABASE,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+    }
 );
 
 module.exports = sequelize;
